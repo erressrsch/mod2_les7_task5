7474591c3ba594e21333026d4e451f5e0c39d350
Szymon Chyc <erressrsch@gmail.com>
2024-01-08 23:11:38 +0100

    Add project files.

---
+++ mod2_les7_task5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace mod2_les7_task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age >= 21)
+            {
+                Console.WriteLine("You can become an MP");
+            }
+            if (age >= 30)
+            {
+                Console.WriteLine("You can become a Senator");
+
+            }
+            if (age >= 30)
+            {
+                Console.WriteLine("You can become Prime Minister");
+            }
+            if (age >= 35)
+            {
+                Console.WriteLine("You can become President");
+            }
+        }
+    }
+}
